14c3a0715e1904902a2e77db934e14bccdd013da
sunJ0120 <sspure1214@gmail.com>
2025-12-30 12:55:10 +0900

    feat: input 파트 조립 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.util.InputParser;
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputParser inputParser = new InputParser();
+        InputView inputView = new InputView(inputParser);
+
+        inputView.inputPurchaseMoney();
+        inputView.inputWinningNumbers();
+        inputView.inputBonusNumbers();
     }
 }
