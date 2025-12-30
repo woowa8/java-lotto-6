fe2f8758f39c98922fc59d9b47fa1fc44962e211
sunJ0120 <sspure1214@gmail.com>
2025-12-30 16:01:48 +0900

    feat: view와 연결을 위한 inputView 마련

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,8 @@ import lotto.domain.Lotto;
 import lotto.util.InputParser;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.awt.*;
+
 public class InputView {
     private final InputParser inputParser;
 
@@ -12,21 +14,30 @@ public class InputView {
         this.inputParser = inputParser;
     }
 
-    public void inputPurchaseMoney() {
+    public int inputPurchaseMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        int money = Integer.parseInt(Console.readLine());
+        String money = Console.readLine();
+
+        int purchaseMoney;
+        try {
+            purchaseMoney = Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 구매 금액은 숫자여야 합니다.");
+            return inputPurchaseMoney();
+        }
+        return purchaseMoney;
     }
 
     public Lotto inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String winningNumbers = Console.readLine();
 
-        Lotto lotto = null;
-        try{
+        Lotto lotto;
+        try {
             lotto = new Lotto(inputParser.parsingNumbers(winningNumbers));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            inputWinningNumbers();
+            return inputWinningNumbers();     // TODO : 재귀 호출 시 return 필수
         }
         return lotto;
     }
@@ -35,12 +46,12 @@ public class InputView {
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = Integer.parseInt(Console.readLine());
 
-        BonusNumber bonus = null;
-        try{
+        BonusNumber bonus;
+        try {
             bonus = new BonusNumber(bonusNumber);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            inputBonusNumbers();
+            return inputBonusNumbers();     // TODO : 재귀 호출 시 return 필수
         }
         return bonus;
     }
