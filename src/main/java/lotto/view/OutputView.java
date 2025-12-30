0bdb78ef289bdd418485757ca767b9702360becb
sunJ0120 <sspure1214@gmail.com>
2025-12-31 01:15:14 +0900

    refactor: 도메인 로직 변경에 따른 view 로직 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,45 +1,43 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
-import lotto.domain.Lottos;
+import lotto.domain.WinningStatistics;
 import lotto.domain.enums.WinningInformation;
 
+import java.util.List;
+
 public class OutputView {
-    public void printLottos(Lottos lottos, int count) {
+    public void printLottos(List<Lotto> lottos, int count) {
         System.out.println(count + "개를 구매했습니다.");
 
-        for (Lotto lotto : lottos.getLottos()) {
+        for (Lotto lotto : lottos) {
             System.out.println(lotto.toString());
         }
         System.out.println();
     }
 
-    public void printStats(Lottos lottos, int money) {
+    public void printStats(int money, WinningStatistics winningStatistics) {
         System.out.println("당첨 통계");
         System.out.println("---");
 
-        for (WinningInformation winningInformation : WinningInformation.values()) {  // TODO : ENUM으로 낮은 것부터 순회하기 (정의 순서 주의)
-            if(winningInformation.getRank() == -1){
+        for (WinningInformation winningInformation : WinningInformation.values()) {
+            if (winningInformation.getRank() == -1) {
                 continue;
             }
-            printInformation(winningInformation, lottos);
+            printInformation(winningInformation, winningStatistics);
         }
 
-        System.out.println("총 수익률은 " + lottos.getRate(money) + "%입니다.");
+        System.out.println("총 수익률은 " + winningStatistics.calculateRate(money) + "%입니다.");
     }
 
-    private void printInformation(WinningInformation winningInformation, Lottos lottos) {
-        boolean isBonus = winningInformation.getIsMatchedBonus();
-        int matchCount = winningInformation.getMatchCount();
-        // TODO : ENUM에서 _를 ,로 formatting 하는 것 기억하기
+    private void printInformation(WinningInformation winningInformation, WinningStatistics winningStatistics) {
         String money = String.format("%,d", winningInformation.getWinningPrice());
-        int count = lottos.getTotalStats().getOrDefault(winningInformation,0);
-        // TODO : 없으면 NULL 이므로 항상 .getOrDefault를 기억하자.
+        int count = winningStatistics.getCount(winningInformation);
 
-        if(isBonus){
-            System.out.println(matchCount + "개 일치, 보너스 볼 일치 " + "(" + money + "원) - " + count + "개");
+        if (winningInformation.getIsMatchedBonus()) {
+            System.out.println(winningInformation.getMatchCount() + "개 일치, 보너스 볼 일치 " + "(" + money + "원) - " + count + "개");
             return;
         }
-        System.out.println(matchCount + "개 일치 " + "(" + money + "원) - " + count + "개");
+        System.out.println(winningInformation.getMatchCount() + "개 일치 " + "(" + money + "원) - " + count + "개");
     }
 }
