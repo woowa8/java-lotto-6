b33b31049723612c5684074b88760284fb08d118
sunJ0120 <sspure1214@gmail.com>
2025-12-30 16:01:13 +0900

    feat: 로또 객체 구현 완료
    1. 예외 처리 : 숫자, 중복, 갯수 검증
    2. 당첨 정보를 계산해서 저장
    3. stream으로 일치 갯수를 depth3 안에서 해결

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,14 @@
 package lotto.domain;
 
+import lotto.domain.enums.WinningInformation;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private WinningInformation winningInformation;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -12,7 +17,8 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateCount(numbers);
-        for(Integer number : numbers){
+        validateDuplicates(numbers);
+        for (Integer number : numbers) {
             validateScope(number);
         }
     }
@@ -27,5 +33,33 @@ public class Lotto {
         if (number < 1 || number > 45) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
+    }
+
+    private void validateDuplicates(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if(set.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+        }
+    }
+
+    private boolean contains(Integer number) {    // TODO : stream 사용을 위해 contains 구현하는거 알아두기..
+        return numbers.contains(number);
+    }
+
+    public void calculateWinningPrice(Lotto winningLotto, BonusNumber bonusNumber) {
+        int cnt = (int) this.numbers.stream()
+                .filter(winningLotto::contains)
+                .count();
+        boolean haveBonus = this.numbers.contains(bonusNumber.getNumber());
+
+        winningInformation = WinningInformation.getWinningInformation(cnt, haveBonus);
+    }
+
+    public WinningInformation getWinningInformation() {
+        return winningInformation;
+    }
+
+    public String toString() {
+        return numbers.toString();
     }
 }
