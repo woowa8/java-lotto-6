ee01d49ca2c4bb155c7a354ea30bde1349cde35e
sunJ0120 <sspure1214@gmail.com>
2025-12-30 15:58:26 +0900

    feat: 추가 번호 저장을 위한 객체 구성
    1. lotto 번호 범위에 들어오는지 보기 위한 예외 구성

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,8 +1,8 @@
 package lotto.domain;
 
 public class BonusNumber {
-    // TODO : 1~45 사이의 숫자가 맞는지 확인 필요
-    private int number;
+    private final int number;
+
     public BonusNumber(int number) {
         isLotto(number);
         this.number = number;
@@ -12,5 +12,9 @@ public class BonusNumber {
         if (number < 1 || number > 45) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
+    }
+
+    public int getNumber() {
+        return number;
     }
 }
