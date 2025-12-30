c342c1186b9644ef6adaf73c3fe37affc9b7a6b3
sunJ0120 <sspure1214@gmail.com>
2025-12-30 16:04:04 +0900

    feat: Application에 조립 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,16 +1,40 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
 import lotto.util.InputParser;
 import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         InputParser inputParser = new InputParser();
         InputView inputView = new InputView(inputParser);
+        OutputView outputView = new OutputView();
 
-        inputView.inputPurchaseMoney();
-        inputView.inputWinningNumbers();
-        inputView.inputBonusNumbers();
+        int money = inputView.inputPurchaseMoney();
+        int cnt = money/1000;    // 로또 구매 개수
+
+        List<Lotto> lottoList = new ArrayList<>();
+
+        for (int i = 0; i < cnt; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoList.add(new Lotto(numbers));
+        }
+        Lottos lottos = new Lottos(lottoList);
+        outputView.printLottos(lottos, cnt);    // 구매한 로또 프린트
+
+        Lotto winningLottos = inputView.inputWinningNumbers();
+        BonusNumber bonus = inputView.inputBonusNumbers();
+
+        lottos.calculateRank(winningLottos, bonus);
+
+        System.out.println();    // 한 칸 띄기
+        outputView.printStats(lottos, money);    // 당첨 통계
     }
 }
